cc4953f7aed126e4f20b32fd6b24a5b8c257606d
Matthew Frankland <developer@matthewfrankland.co.uk>
2020-04-06 15:09:54 +0100

    removed duplicates

---
+++ Project/study-planner/controllers/assets/button/styles 2.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+/*
+  ==========================================
+   Title: assets/button/style
+   Author: Matthew Frankland
+  ==========================================
+*/
+
+export default StyleSheet.create( {
+  container: {
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+    marginBottom: 12,
+    paddingVertical: 5,
+    borderRadius: 4,
+    borderWidth: StyleSheet.hairlineWidth
+  },
+  text: {
+    textAlign: "center",
+    height: 20
+  },
+  textColor: {
+    color: "white",
+    fontWeight: 'bold'
+  }
+} );
---
+++ Project/study-planner/controllers/home-components/styles 2.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+
+/*
+  ==========================================
+   Title: home-components/style
+   Author: Matthew Frankland
+  ==========================================
+*/
+
+export default StyleSheet.create( {
+  container: {
+    flex: 1,
+    backgroundColor: '#0B345A',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInput: {
+    height: 30,
+    borderColor: "#BEBEBE",
+    borderBottomWidth: 0.4,
+    color: '#FFF',
+    marginBottom: 30
+  },
+  logo: {
+    flex: 1,
+    width: "100%",
+    resizeMode: "contain",
+    alignSelf: "center"
+  },
+  form: {
+    flex: 1,
+    justifyContent: "center",
+    width: "80%"
+  }
+} );
