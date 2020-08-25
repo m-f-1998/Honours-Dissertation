46de8aae70cf7e81d7e98862e88ae832b204191d
Matthew Frankland <developer@matthewfrankland.co.uk>
2020-08-25 11:57:08 +0100

    Update constants.php

--- Project/Controller/dbConnection/constants.php
+++ Project/Controller/dbConnection/constants.php
@@ -6,9 +6,9 @@
  * @var [string]
  */
 
-define('DB_HOST', 'db813088185.hosting-data.io');
-define('DB_USERNAME', 'dbo813088185');
-define('DB_PASSWORD', 'Jyccuj-7qydno-rejpeb');
-define('DB_NAME', 'db813088185');
+define('DB_HOST', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
 
 ?>
